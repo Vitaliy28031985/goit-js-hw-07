f5ffd14894b0f920895afb142fee7fc1d0840300
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-04-25 09:32:40 +0300

    12

---
+++ goit-js-hw-07/src/js/01-gallery.js
@@ -0,0 +1,22 @@
+
+import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createGallery = () => {
+  const galleryListElements = galleryItems.map(({preview, description, original,}) => `<a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}" src="${original}" data-source="${preview}"></a>`).join('');
+   
+   galleryContainer.insertAdjacentHTML('afterbegin', galleryListElements);  
+}
+createGallery();
+
+document.querySelector('a.gallery__image').onclick = () => {
+
+	basicLightbox.create(`
+		<img width="1400" height="900" src="${description}">
+	`).show()
+
+}
+
---
+++ goit-js-hw-07/src/js/02-lightbox.js
@@ -0,0 +1,14 @@
+
+import { galleryItems } from './gallery-items.js';
+console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+const createGallery = () => {
+  const galleryListElements = galleryItems.map(({preview, description, original,}) => `<li><a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}" src="${preview}"></a></li>`).join('');
+   
+   galleryContainer.insertAdjacentHTML('afterbegin', galleryListElements);  
+}
+createGallery();
+
