b4c8bf44b5abc13846b25687320dcfba5f60fc2c
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-04-28 18:17:53 +0300

    Галерея 1

--- goit-js-hw-07/src/js/01-gallery.js
+++ goit-js-hw-07/src/js/01-gallery.js
@@ -1,30 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 
 const createGallery = () => {
-  const galleryListElements = galleryItems.map(({preview, description, original,}) =>
-  `<a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}" src="${original}" data-source="${preview}"></a>`).join('');
-   
-   galleryContainer.insertAdjacentHTML('afterbegin', galleryListElements);  
-}
+  const galleryListElements = galleryItems
+    .map(
+      ({ preview, description, original }) =>
+        `<a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}" src="${preview}" data-source="${original}"></a>`
+    )
+    .join("");
 
-
-const handleCloseByEscp = (event) => {
-  const a = document.querySelector('a');
-  a.eddEventListener('keydown', (e) => e.preventDefault());
-  makeLightbox(
-    event.dataset.preview,
-  ); 
-}
-basicLightbox.create(
-  createGallery(),{
-onShow: () => {
-window.addEventListener('keydown', handleCloseByEscp)
-},
-onClose: () => {
-window.removeEventListener('keydown', handleCloseByEscp)
-}
-})
+  galleryContainer.insertAdjacentHTML("afterbegin", galleryListElements);
+};
+createGallery();
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  basicLightbox
+    .create(
+      `
+  <img src="${e.target.dataset.source}" width="800" height="600">
+`,
+      {
+        onShow: () => {},
+        onClose: () => {},
+      }
+    )
+    .show();
+});
