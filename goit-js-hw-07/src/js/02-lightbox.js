e75cbaaae71d342e1b4f3a788365f36b7270a7d1
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-04-29 13:41:55 +0300

    Чорновий варіант галереї 2

--- goit-js-hw-07/src/js/02-lightbox.js
+++ goit-js-hw-07/src/js/02-lightbox.js
@@ -7,9 +7,19 @@ console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 console.log(galleryContainer);
 const createGallery = () => {
-  const galleryListElements = galleryItems.map(({preview, description, original,}) => `<li><a class="gallery__item" href="${original}"><img class="gallery__image" alt="${description}" src="${preview}"></a></li>`).join('');
+  const galleryListElements = galleryItems.map(({preview, description, original,}) => `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" alt="${description}" src="${preview}" data-source="${original}"></a></li>`).join('');
    
    galleryContainer.insertAdjacentHTML('afterbegin', galleryListElements);  
 }
 createGallery();
 
+galleryContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	e.target.dataset.source;
+}).open();
+});
